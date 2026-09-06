19946c4335f8b3646f15e3cbaa3e7abb2c0a9a49
Dominic Renda <dzr0071@auburn.edu>
2026-09-06 16:43:40 -0500

    Add starter objects and fix variable declaration
    Fixed typo in variable declaration and added starter objects.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,16 @@ using namespace std;
 int main()
 {
 	//create a few starter objects
+	Starter obj1;
+	Starter obj2l
+	
 	//add some values to test out your values methods
+	obj1.setValue(10);
+	obj2.setValue(25);
+	
 	//print out all the values for each object to validate your code
+	cout << obj1.getValue() << endl;
+	cout << obj2.getValue() << endl;
+
+	return 0;
 }
